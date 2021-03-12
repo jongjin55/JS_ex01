bf4b37c867ec5dc7fb648ed26881a0968f5a27d6
jongjin55 <dmer11@naver.com>
2021-03-13 00:33:51 +0900

    ex01

---
+++ ex01.js
@@ -0,0 +1,16 @@
+const nicoInfo = {
+
+    name : "Nicol",
+    age : 33,
+    gender : "Male",
+    isHandsome : true,
+    favMovies : ["Alon g the gods", "LOTR"],
+    favFood : [{name : "Kimchi", fatty : false}, {name : "Cheese burger", fatty : true}]
+}
+const monday = "Mon";
+
+nicoInf
+
+console.log(nicoInfo.gender);
+
+alert(1);
