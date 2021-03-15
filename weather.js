d16cf5dc982b14f7b48599cf799cd8e729a697b7
jongjin55 <dmer11@naver.com>
2021-03-16 00:51:40 +0900

    바닐라 자바스크립트 예제 끝

---
+++ weather.js
@@ -0,0 +1,53 @@
+const weather = document.querySelector(".js-weather");
+const API_KEY = "241051bf13976dd3ddf8b8d9f24755e";
+const COORDS = 'coords';
+
+function getWeather(lat, lng) {
+    fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=${API_KEY}&units=metrics`)
+    .then(function(response) {
+        return console.log(response.json);
+    }).then(function(json) {
+       const tem = json.main.temp;
+       const place = json.name;
+       weather.innerText = `${tem}@ ${place}`;
+    });
+}
+
+function init() {
+    loadCoords();
+}
+init();
+
+
+function saveCoords(coordsObj) {
+    localStorage.setItem(COORDS, JSON.stringify(coordsObj));
+}
+
+function handleGeoSucces(position) {
+    const latitude = position.coords.latitude;
+    const longitude = position.coords.longitude;
+    const coordsObj = {
+        latitude,
+        longitude
+    };
+    saveCoords(coordsObj);
+    getWeather(latitude, longitude);
+}
+
+function handleGeoError(){
+    console.log("Can't access geo location");
+}
+
+function askForCoords() {
+    navigator.geolocation.getCurrentPosition(handleGeoSucces, handleGeoError);
+}
+
+function loadCoords() {
+    const loadedCoords = localStorage.getItem(COORDS);
+    if(loadedCoords === null) {
+        askForCoords();
+    }else {
+        const parseCoords = JSON.parse(loadedCoords);
+        getWeather(parseCoords.latitude, parseCoords.longitude);
+    }
+}
